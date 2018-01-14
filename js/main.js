0700d44b34cd2fe0ca8634dd7ec56c4a390854a9
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2018-01-14 22:45:38 +0100

    tetahedron final

--- js/main.js
+++ js/main.js
@@ -1,14 +1,226 @@
+// zmienne globalne
 
-drawRectangles();
+var gl_canvas;
 
-function drawRectangles() {
-  var canvas = $('canvas#view');
-  var context = canvas.get(0).getContext("2d");
+var gl_ctx;
 
-  context.fillStyle = "#EE7070";
-  context.fillRect(50, 50, 160, 80);
-  context.fillStyle = "#70CB55";
-  context.fillRect(90, 80, 140, 70);
-  context.fillStyle = "#5C79AB";
-  context.fillRect(130, 110, 120, 60);
+
+
+var _triangleVertexBuffer;
+var _triangleFacesBuffer;
+var _position;
+var _color; var _PosMatrix;
+var _MovMatrix;
+var _ViewMatrix;
+var _matrixProjection;
+var _matrixMovement;
+var _matrixView;
+
+
+var rotationSpeed = 0.001;
+var zoomRatio = -6;
+var alreadyAnimating = false;
+
+var X, Y, Z;
+
+// funkcja główna
+function runWebGL () {
+   getRotation();
+
+   gl_canvas = document.getElementById("view");
+   gl_ctx = gl_getContext(gl_canvas);
+
+   gl_initShaders();
+   gl_initBuffers();
+   gl_setMatrix();
+   gl_draw();
+}
+
+function getRotation() {
+   X = $("#rotateX").get(0).checked;
+   Y = $("#rotateY").get(0).checked;
+   Z = $("#rotateZ").get(0).checked;
+}
+
+function gl_getContext (canvas) {
+
+  try {
+      var ctx = canvas.getContext("webgl");
+      ctx.viewportWidth = canvas.width;
+      ctx.viewportHeight = canvas.height;
+   } catch (e) {}
+
+   if (!ctx) {
+      document.write('Unable to initialize WebGL context.')
+   }
+   return ctx;
+}
+
+
+function gl_initShaders () {
+   var vertexShader = `\n
+      attribute vec3 position;\n\
+      uniform mat4 PosMatrix;\n\
+      uniform mat4 MovMatrix;\n\
+      uniform mat4 ViewMatrix; \n\
+      attribute vec3 color;\n\
+      varying vec3 vColor;\n\
+
+      void main(void) {\n\
+         gl_Position = PosMatrix * ViewMatrix * MovMatrix * vec4(position, 1.);\n\
+         vColor = color;\n\
+      }`;
+
+    var fragmentShader = `\n\
+      precision mediump float;\n\
+      varying vec3 vColor;\n\
+
+      void main(void) {\n\
+         gl_FragColor = vec4(vColor, 1.);\n\
+
+      }`;
+
+    var getShader = function(source, type, typeString) {
+      var shader = gl_ctx.createShader(type);
+      gl_ctx.shaderSource(shader, source);
+      gl_ctx.compileShader(shader);
+
+       if (!gl_ctx.getShaderParameter(shader, gl_ctx.COMPILE_STATUS)) {
+         alert('error in' + typeString);
+         return false;
+
+      }
+      return shader;
+   };
+
+   var shader_vertex = getShader(vertexShader, gl_ctx.VERTEX_SHADER, "VERTEX");
+   var shader_fragment = getShader(fragmentShader, gl_ctx.FRAGMENT_SHADER, "FRAGMENT");
+
+   var shaderProgram = gl_ctx.createProgram();
+   gl_ctx.attachShader(shaderProgram, shader_vertex);
+   gl_ctx.attachShader(shaderProgram, shader_fragment);
+   gl_ctx.linkProgram(shaderProgram);
+
+   _PosMatrix = gl_ctx.getUniformLocation(shaderProgram, "PosMatrix");
+   _MovMatrix = gl_ctx.getUniformLocation(shaderProgram, "MovMatrix");
+   _ViewMatrix = gl_ctx.getUniformLocation(shaderProgram, "ViewMatrix");
+
+   _position = gl_ctx.getAttribLocation(shaderProgram, "position");
+   _color = gl_ctx.getAttribLocation(shaderProgram, "color");
+
+   gl_ctx.enableVertexAttribArray(_position);
+   gl_ctx.enableVertexAttribArray(_color);
+   gl_ctx.useProgram(shaderProgram);
+}
+
+
+function gl_initBuffers () {
+
+   var tetahedronVerticles = [
+      1, 1, 1,
+      1, 0, 0,
+
+      -1, -1, 1,
+      0, 1, 0,
+
+      -1, 1, -1,
+      0, 0, 1,
+
+      1, -1, -1,
+      1, 1, 0
+   ];
+
+  _tetahedronVertexBuffer = gl_ctx.createBuffer();
+
+   gl_ctx.bindBuffer(gl_ctx.ARRAY_BUFFER, _tetahedronVertexBuffer);
+
+   gl_ctx.bufferData(gl_ctx.ARRAY_BUFFER,
+
+                     new Float32Array(tetahedronVerticles),
+
+                     gl_ctx.STATIC_DRAW);
+
+
+
+    var tetahedronFaces = [
+
+      0, 1, 2,
+      0, 2, 3,
+      0, 1, 3,
+      1, 2, 3,
+   ];
+
+   _tetahedronFacesBuffer = gl_ctx.createBuffer();
+
+   gl_ctx.bindBuffer(gl_ctx.ELEMENT_ARRAY_BUFFER, _tetahedronFacesBuffer);
+
+   gl_ctx.bufferData(gl_ctx.ELEMENT_ARRAY_BUFFER,
+
+                     new Uint16Array(tetahedronFaces),
+
+                     gl_ctx.STATIC_DRAW);
+
+}
+
+function gl_setMatrix () {
+     _matrixProjection = MATRIX.getProjection(40, gl_canvas.width/gl_canvas.height, 1, 100);
+     _matrixMovement = MATRIX.getIdentityMatrix();
+     _matrixView = MATRIX.getIdentityMatrix();
+
+     MATRIX.translateZ(_matrixView, zoomRatio);
+}
+
+function gl_draw() {
+   gl_ctx.clearColor(0.0, 0.0, 0.0, 0.0);
+
+   gl_ctx.enable(gl_ctx.DEPTH_TEST);
+
+   gl_ctx.depthFunc(gl_ctx.LEQUAL);
+
+   gl_ctx.clearDepth(1.0);
+
+   var timeOld = 0;
+
+    var animate = function (time) {
+
+      var dAngle = rotationSpeed * (time - timeOld);
+
+      if (X) {
+         MATRIX.rotateX(_matrixMovement, dAngle);
+      }
+
+      if (Y) {
+         MATRIX.rotateY(_matrixMovement, dAngle);
+      }
+
+      if (Z) {
+         MATRIX.rotateZ(_matrixMovement, dAngle);
+      }
+
+      timeOld = time;
+
+      gl_ctx.viewport(0.0, 0.0, gl_canvas.width, gl_canvas.height);
+      gl_ctx.clear(gl_ctx.COLOR_BUFFER_BIT | gl_ctx.DEPTH_BUFFER_BIT);
+
+      gl_ctx.uniformMatrix4fv(_PosMatrix, false, _matrixProjection);
+      gl_ctx.uniformMatrix4fv(_MovMatrix, false, _matrixMovement);
+      gl_ctx.uniformMatrix4fv(_ViewMatrix, false, _matrixView);
+
+      gl_ctx.vertexAttribPointer(_position, 3, gl_ctx.FLOAT, false, 4*(3+3), 0);
+      gl_ctx.vertexAttribPointer(_color, 3, gl_ctx.FLOAT, false, 4*(3+3), 3*4);
+
+      gl_ctx.bindBuffer(gl_ctx.ARRAY_BUFFER, _tetahedronVertexBuffer);
+      gl_ctx.bindBuffer(gl_ctx.ELEMENT_ARRAY_BUFFER, _tetahedronFacesBuffer);
+
+      gl_ctx.drawElements(gl_ctx.TRIANGLES, 4*3, gl_ctx.UNSIGNED_SHORT, 0);
+      gl_ctx.flush();
+
+      window.requestAnimationFrame(animate);
+   };
+
+  if(!alreadyAnimating) {
+    animate(0);
+    alreadyAnimating = true;
+  }
+
 }
